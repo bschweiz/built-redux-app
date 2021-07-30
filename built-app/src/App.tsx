37fe01056a02d5e241793b03503cf57ca159f016
Benjamin Schweizer <bschweiz@gmail.com>
2021-07-30 11:31:06 -0500

    add ad-grid packages and boilerplate

--- built-app/src/App.tsx
+++ built-app/src/App.tsx
@@ -2,56 +2,74 @@ import React from 'react';
 import logo from './built-logo.png';
 import { Counter } from './features/counter/Counter';
 import './App.css';
+import {AgGridColumn, AgGridReact} from 'ag-grid-react';
+
+import 'ag-grid-community/dist/styles/ag-grid.css';
+import 'ag-grid-community/dist/styles/ag-theme-alpine.css';
 
 function App() {
+  const rowData = [
+    { make: "Toyota", model: "Celica", price: 35000 },
+    { make: "Ford", model: "Mondeo", price: 32000 },
+    { make: "Porsche", model: "Boxter", price: 72000 }
+  ];
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <Counter />
-        <p>
-          Welcome to this <code>built</code> challenge app.
-        </p>
-        <span>
-          <span>Learn </span>
-          <a
-            className="App-link"
-            href="https://reactjs.org/"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            React
-          </a>
-          <span>, </span>
-          <a
-            className="App-link"
-            href="https://redux.js.org/"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Redux
-          </a>
-          <span>, </span>
-          <a
-            className="App-link"
-            href="https://redux-toolkit.js.org/"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Redux Toolkit
-          </a>
-          ,<span> and </span>
-          <a
-            className="App-link"
-            href="https://react-redux.js.org/"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            React Redux
-          </a>
-        </span>
-      </header>
+    <div className="ag-theme-alpine" style={{ height: 400, width: 600 }}>
+      <AgGridReact
+        rowData={rowData}>
+        <AgGridColumn field="make"></AgGridColumn>
+        <AgGridColumn field="model"></AgGridColumn>
+        <AgGridColumn field="price"></AgGridColumn>
+      </AgGridReact>
     </div>
+    // <div className="App">
+    //   <header className="App-header">
+    //     <img src={logo} className="App-logo" alt="logo" />
+    //     <Counter />
+    //     <p>
+    //       Welcome to this <code>built</code> challenge app.
+    //     </p>
+    //     <span>
+    //       <span>Learn </span>
+    //       <a
+    //         className="App-link"
+    //         href="https://reactjs.org/"
+    //         target="_blank"
+    //         rel="noopener noreferrer"
+    //       >
+    //         React
+    //       </a>
+    //       <span>, </span>
+    //       <a
+    //         className="App-link"
+    //         href="https://redux.js.org/"
+    //         target="_blank"
+    //         rel="noopener noreferrer"
+    //       >
+    //         Redux
+    //       </a>
+    //       <span>, </span>
+    //       <a
+    //         className="App-link"
+    //         href="https://redux-toolkit.js.org/"
+    //         target="_blank"
+    //         rel="noopener noreferrer"
+    //       >
+    //         Redux Toolkit
+    //       </a>
+    //       ,<span> and </span>
+    //       <a
+    //         className="App-link"
+    //         href="https://react-redux.js.org/"
+    //         target="_blank"
+    //         rel="noopener noreferrer"
+    //       >
+    //         React Redux
+    //       </a>
+    //     </span>
+    //   </header>
+    // </div>
   );
 }
 
